2de920e215e9aa20bd616eef8e1004a7fd143727
HostileZeth <sergeyataev@gmail.com>
2019-02-01 09:28:00 +0400

    Update Address.java
    constructors added

--- Task2/src/barBossHouse/Address.java
+++ Task2/src/barBossHouse/Address.java
@@ -9,6 +9,43 @@ package barBossHouse;
  *
  * @author rLogic
  */
-public class Address {
+public class Address { //Immutable
     private String city;
+    private String street;
+    private int postIndex;
+    private int buildingNumber;
+    private char buildingChar;
+    private int roomNumber;
+    
+    static private String DEFAULT_STRING_VALUE ="";
+    static private int DEFAULT_INT_VALUE = -1;
+    
+    public Address ()
+    {
+        city = DEFAULT_STRING_VALUE;
+        street = DEFAULT_STRING_VALUE;
+        postIndex = DEFAULT_INT_VALUE;
+        buildingNumber = DEFAULT_INT_VALUE;
+        buildingChar = (char)DEFAULT_INT_VALUE;
+        roomNumber = DEFAULT_INT_VALUE;
+    }
+    
+    public Address (String street, int buildingNumber, char buildingChar, int roomNumber)
+    {
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.buildingChar = buildingChar;
+        this.roomNumber = roomNumber;
+    }
+    
+    public Address (String city, String street, int postIndex, int buildingNumber, char buildingChar, int roomNumber)
+    {
+        this.city = city;
+        this.street = street;
+        this.postIndex = postIndex;
+        this.buildingNumber = buildingNumber;
+        this.buildingChar = buildingChar;
+        this.roomNumber = roomNumber;
+    }
+    
 }
